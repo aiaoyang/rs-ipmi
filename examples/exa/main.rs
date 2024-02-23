2ef5ce4c7a961cb564dbd6444541b4c9b293668e
yangjiangdong <jiangdong.yang@baishan.com>
2024-02-23 09:04:27 +0800

    example

--- examples/exa/main.rs
+++ examples/exa/main.rs
@@ -7,16 +7,18 @@ fn main() {
         .unwrap_or_else(|e| println!("{e:?}"));
 
     let resp = c
-        .send_raw_request(&[0x0A, 0x43, 0, 0, 0, 0, 0, 0xff])
+        .send_raw_request(&[0x0A, 0x23, 0x00, 0x00, 0xff, 0x0ff, 0x00, 0x0e])
+        // .send_raw_request(&[0x0A, 0x43, 0, 0, 0, 0, 0, 0xff])
         .unwrap();
 
     println!("resp: {:?}", resp);
     let data = resp.data;
-    if !data.is_empty() {
-        // println!("respdata: {:x?},len: {}", data, data.len());
-        let entry = rust_ipmi::Entry::parse(&data[2..]).unwrap();
-        println!("entry: {entry}");
-    } else {
-        println!("no data")
-    }
+    println!("{:?}", data);
+    // if !data.is_empty() {
+    //     // println!("respdata: {:x?},len: {}", data, data.len());
+    //     let entry = rust_ipmi::Entry::parse(&data[2..]).unwrap();
+    //     println!("entry: {entry}");
+    // } else {
+    //     println!("{:?}", data);
+    // }
 }
